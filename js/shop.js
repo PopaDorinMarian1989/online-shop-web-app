7d5b87eed65f6622b2b8c5e4b44b89766c748fef
popa dorin <dorinmarian89@gmail.com>
2020-01-06 21:20:31 +0200

    displayng products from api in shop page

--- js/shop.js
+++ js/shop.js
@@ -4,29 +4,39 @@ window.Shop = {
     getProduct: function () {
         $.ajax({
             url: Shop.API_BASE_URL + "/products"
+            //default ajax method : "GET"
         }).done(function (response) {
             console.log(response);
+            Shop.diplayProduct(response.content);
 
         });
 
     },
     getProductHtml: function (product) {
-return `<div class="col-md-3 col-sm-6">
+        return `<div class="col-md-3 col-sm-6">
                     <div class="single-shop-product">
                         <div class="product-upper">
                             <img src="img/product-2.jpg" alt="">
                         </div>
-                        <h2><a href="">$(product.name)</a></h2>
+                        <h2><a href="">${product.name}</a></h2>
                         <div class="product-carousel-price">
-                            <ins>$$(product.price)</ins> 
+                            <ins>$${product.price}</ins> 
                         </div>  
                         
                         <div class="product-option-shop">
-                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="$(product.id)" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id="${product.id}" rel="nofollow" href="/canvas/shop/?add-to-cart=70">Add to cart</a>
                         </div>                       
                     </div>
                 </div>`
+    },
+
+    diplayProduct: function (products) {
+        var productsHtml = "";
+        products.forEach(oneproduct => productsHtml += Shop.getProductHtml(oneproduct));
+        $(".single-product-area .row:first-child") .html(productsHtml);
+
     }
+
 };
 
 Shop.getProduct();
